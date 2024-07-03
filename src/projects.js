5b12939d309444e70722ac2b745f14371e678afd
Ethan Farber <edf1247@gmail.com>
2024-07-02 21:26:54 -0400

    styled project form

--- src/projects.js
+++ src/projects.js
@@ -25,32 +25,66 @@ function addProjectDom() {
 
     const formContainer = document.createElement("div");
     const formTitle = document.createElement("div");
-    const projName = document.createElement("div");
+    const projName = document.createElement("form");
     const projInput = document.createElement("input");
     const projInputLabel = document.createElement("div");
-    const submit = document.createElement("div");
+    const submitCont = document.createElement("div");
+    const submitText = document.createElement("div");
+    const plusIcon = document.createElement("div");
+    const wrapper = document.createElement("div");
+
+    wrapper.id = "wrapper";
+
+    plusIcon.id = "plus-icon";
+    submitText.innerHTML = "Create";
+    submitText.id = "submit-project";
+    submitCont.id = "submit-cont";
 
     formContainer.id = "project-form-container";
     formTitle.id = "project-title-container";
     formTitle.innerHTML = "Create Project";
     
+    projName.id = "project-body";
     projInputLabel.innerHTML = "Project Name:";
+    projInputLabel.id = "proj-input-label";
     projInput.type = "text";
+    projInput.id = "proj-input";
+
+    
+
+    wrapper.appendChild(projInputLabel);
+    wrapper.appendChild(projInput);
+
+    submitText.appendChild(plusIcon);
+    submitCont.appendChild(submitText);
+    
 
     formContainer.appendChild(formTitle);
-    projName.appendChild(projInputLabel);
-    projName.appendChild(projInput);
+    projName.appendChild(wrapper);
     formContainer.appendChild(projName);
-    formContainer.appendChild(submit);
-
+    projName.appendChild(submitCont);
+    
 
     page.appendChild(blur);
     blur.appendChild(formContainer);
+}
+
+function processProjectInput(){
+    const projectName = document.getElementById("proj-input").value;
+    const projectSubmit = document.getElementById("submit-project");
+
+    projectSubmit.addEventListener("click", function(){
+        projectList.push(projectName);
+        console.log(projectList);
+    });
+
+
 }
 
 export function renderAddProjectForm() {
     let button = document.getElementById("project-button");
     button.addEventListener("click", function(){
         addProjectDom();
+        processProjectInput();
     });
 }
