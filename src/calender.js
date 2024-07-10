3a8e92971a908a18f80f0e5a097867a80de95c7d
Ethan Farber <edf1247@gmail.com>
2024-07-09 22:57:03 -0400

    added function to count the number of objects due today

--- src/calender.js
+++ src/calender.js
@@ -1,5 +1,17 @@
 import { todoArr } from "./todo";
 
-function updateCalender(){
-    
+function countToday(){
+    let counter = 0;
+    for(let i = 0; i < todoArr.length; i++){
+        let todoDate = todoArr[i].deadline;
+
+        let todaysDate = new Date();
+
+        if(todoDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)){
+            counter++;
+        }
+    }
+    let numToday = counter;
+    return numToday;
 }
+
